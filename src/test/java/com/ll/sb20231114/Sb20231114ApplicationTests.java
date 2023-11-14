4e3ea3aa718cad07d19564df426af3125efcd33d
J-Cheol <lunaris0206@naver.com>
2023-11-14 09:22:58 +0900

    setting

---
+++ src/test/java/com/ll/sb20231114/Sb20231114ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.sb20231114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20231114ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
