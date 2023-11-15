348e1b3bfabc2c4087b5fdca3243b7a441be9707
J-Cheol <lunaris0206@naver.com>
2023-11-15 11:42:01 +0900

    step 28, fomethod=post

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
@@ -21,7 +22,7 @@ public class ArticleController
         return "article/write";
     }
 
-    @GetMapping("/article/doWrite")
+    @PostMapping("/article/write")
     @ResponseBody
     RsData<Article> doWrite(String title, String body)
     {
